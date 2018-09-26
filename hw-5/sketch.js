706e70365189c4096d8e17e4fcb9315eb9bd7b4d
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2018-09-26 10:12:08 -0600

    starting hw-5

---
+++ hw-5/sketch.js
@@ -0,0 +1,37 @@
+Dani Briggs, 50
+
+//creating a new character
+//seting it up
+function setup() {
+  createCanvas( windowWidth, 500 );
+
+}
+
+//seting the varables
+let bodyAngle = 0;
+
+
+//creating the draw function
+function draw() {
+    background( 'pink' );
+    //no cursor on screen
+    noCursor();
+
+    //The Updating Values
+
+
+    //***
+    // character sandbox
+    //***
+    push();
+
+    // character follows mouse
+    translate( mouseX, mouseY );
+
+
+
+
+
+
+
+}
