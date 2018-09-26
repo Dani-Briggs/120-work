679cfa5b0e4f54216666899a8e48bd3ec7fdf155
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2018-09-26 10:54:01 -0600

    starting charcter body, head and eye

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -9,6 +9,9 @@ function setup() {
 
 //seting the varables
 let bodyAngle = 0;
+let bodyRotateRate = 0;
+let bodyWidth = 90;
+let bodyHeight = 90;
 
 
 //creating the draw function
@@ -18,6 +21,11 @@ function draw() {
     noCursor();
 
     //The Updating Values
+    //the body rotation rate
+    bodyRotateRate = ( mouseY * 0.5 ) - 20;
+    // body angle to equal itself plus rotation rate
+    bodyAngle = bodyAngle + bodyRotateRate;
+
 
 
     //***
@@ -28,9 +36,44 @@ function draw() {
     // character follows mouse
     translate( mouseX, mouseY );
 
+    //***
+    //BODY
+    //***
+    push();
+
+    strokeWeight(8);
+    fill( 'rgb(255, 116, 28)');
+    rotate( radians( bodyAngle ));
+    ellipse( 0, 0, bodyWidth, bodyHeight );
+
+    //***
+    //EYE
+    //***
+    push();
+
+    strokeWeight(4);
+    fill(255);
+    //move to center of body
+    translate( bodyWidth * -0.2, bodyHeight * -0.2 );
+    ellipse( 0, 0, bodyWidth * 0.33, bodyHeight * 0.33 );
+
+    //iris
+    strokeWeight(2);
+    fill( 'rgb(45, 255, 123)');
+    ellipse( 0, 0, 10 );
+    //pupul
+    fill(0);
+    ellipse( 0, 0, 5 );
+
+    pop();
+    pop(); // end of body 
 
 
 
+
+
+
+    pop(); //End of character
 
 
 
