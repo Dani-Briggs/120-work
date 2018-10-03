13db715eaf35105bb4dc04dc2caa355b2a2b9f52
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2018-10-03 16:24:48 -0600

    finshed sketch

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,27 +1,56 @@
 //Defining the varables
 
+var pos_x;
+var pos_y;
 
+var circle_min = 20;
+var circle_max;
 
-
-
+//setup
 function setup(){
 
   createCanvas(windowWidth, 500);
-  background('teal');
-  frameRate(15);
+  background(0);
+  frameRate(10);
+
+  pos_x = width * 0.5;
+  pos_y = height * 0.5;
+
+  circle_max = height - 40;
+
+
 }
 
-
+//draw
 function draw(){
 
-  var size = random( height-10, 20);
+  noCursor();
+  //size and stroke color random
+  //var size = random( height-40, 20);
   var color = floor( random(256) );
-  var circleMove = map(mouseX, mouseY, 0, width);
-  fill('pink');
+  var circle_size = map(mouseX, 0, width, circle_min, circle_max)
 
+  //colors for circle fill
+  var cirRed = floor( random(256));
+  var cirBlue = floor( random (256));
+  var cirGreen = floor( random(256));
+
+  //Constrain function
+  var leftWall = 25;
+  var rightWall = width - 25;
+  var xc = constrain(mouseX, leftWall, rightWall);
+  //walls
+  stroke(255);
+  line(leftWall, 0, leftWall, height);
+  line(rightWall, 0, rightWall, height);
+
+
+  //circle
+  fill(cirRed,cirBlue,cirGreen);
   stroke(color,color,color);
   strokeWeight(10);
-  ellipse(circleMove, size);
+  ellipse( xc, mouseY, circle_size);
+
 
 
 }
