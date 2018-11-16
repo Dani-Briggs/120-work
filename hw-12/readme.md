6227ca255725816cd0609b38a29208d1fc99d40b
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2018-11-16 09:59:51 -0700

    finshed sketch and readme

---
+++ hw-12/readme.md
@@ -0,0 +1,26 @@
+Dani Briggs, 50
+
+[My sketch](https://dani-briggs.github.io/120-work/hw-12/)
+
+# HW-12
+
+## Summary
+
+I struggled with this assignment. I had an idea of what to do, but I had a lot of problems trying to get it to do what I want to do. I wanted to have two balls, with two different colors, to come together when someone pressed and hold the mouse, the balls would change color, and then when the person let go of the mouse, the balls would bounce off and go back to there orginal color. I couldn't get that to happen, because I ran out of time and needed to get this assignment turned in.
+
+
+## Steps
+
+ I tested out multple things on the examples on the website first and to see what I needed to for my sketch to work.. I setup the class and all the functions in the class. Then, I setup the varables and the rest of the functions. Then I ran into the same problem as the last assignment; it wouldn't show up. I put my issue on github and got help for that. I also ask for help on how I should get my mouse function to work like I wanted to. I was able to get my sketch to show, but couldn't figure out how to get the mouse to work.
+
+## Disscusion
+
+  I want to talk about how I would get the mouse to work as I wanted to.
+
+## Problems
+
+  I struggle with my sketch not showing up and trying to get my mouse function to work as intended.
+
+## Helping Others
+
+  Didn't get a chance to help.
--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,1 +1,141 @@
+//setting up the varables
 
+let balls = [];
+let num_of_balls = 2;
+
+
+//setup
+function setup(){
+    createCanvas(windowWidth, 600);
+
+    let init_x = 50;
+    let init_y = 50;
+    for (let i = 0; i < num_of_balls; i++){
+      balls.push(new Ball(init_x, init_y));
+
+      init_x += 100;
+      if (init_x > width){
+        init_x = 50;
+        init_y += 150;
+      }
+    }
+
+
+}
+
+
+//draw
+function draw(){
+  background('rgb(166, 32, 66)');
+
+  for (let i = 0; i < balls.length; i++){
+    balls[i].ballCheck(balls, i);
+    balls[i].wallCheck();
+    balls[i].move();
+    balls[i].display();
+    balls[i].mouseCheck();
+  }
+
+
+}
+
+function mousePressed() {
+  for(let i = balls.length - 1; i >= 0; i--){
+    let comeTogether = balls[i].mouseCheck();
+    if (comeTogether){
+      balls[i].move(mouseX, mouseY);
+
+    }
+
+      }
+    }
+
+
+
+///////
+// creating the class
+//////
+
+class Ball {
+  constructor(x, y, size){
+    this.color_one = 'blue';
+    this.color_two = 'yellow';
+    this.size = (20, 50);
+    this.rad = this.size / 2;
+    this.posX = x;
+    this.posY = y;
+    this.deltaX = random(-5,5);
+    this.deltaY = random(-5,5);
+  }
+
+  display(){
+    push();
+
+    stroke(0);
+    strokeWeight(5);
+    fill(this.color_one);
+    translate(this.posX, this.posY);
+    ellipse(0, 0, this.size);
+
+    pop();
+
+    push();
+
+    stroke(0);
+    strokeWeight(5);
+    fill(this.color_two);
+    translate((this.posX + width) * 0.5, (this.posY + height) * 0.5);
+    ellipse(0, 0, this.size);
+
+    pop();
+
+  }
+
+  move(){
+    this.posY += this.deltaY
+    this.posX += this.deltaX
+  }
+
+  wallCheck(){
+    if (this.posX + this.rad >= width || this.posX + this.rad <= 0){
+      this.deltaX *= -1;
+    }
+    if (this.posX + this.rad >= height || this.posY + this.rad <= 0){
+      this.deltaY *= -1;
+    }
+}
+  ballCheck(otherBall, myVar){
+    for (let n = 0; n < otherBall.length; n++){
+           if (myVar !== n){
+           let other = otherBall[n];
+           let d = dist(this.posX, this.posY, other.posX, other.posY);
+           let combR = this.rad + other.rad;
+
+               if (d <= combR){
+                   this.deltaX *= -1;
+                   this.deltaY *= -1;
+
+                   if (this.color_one == 'rgb(17, 30, 108)'){
+                     this.color_two = 'rgb(245, 245, 0)';
+                   }else {
+                     this.color_one = 'rgb(124, 252, 0)';
+                     this.color_two = this.color_one;
+
+                     }
+                   }
+
+               }
+           }
+       }
+
+  mouseCheck(){
+
+    let d = dist(this.posX, this.posY, mouseX, mouseY);
+    if (d > this.rad) {
+      return true;
+    }else {
+        return false;
+      }
+    }
+
+}
