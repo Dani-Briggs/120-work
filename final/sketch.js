831ee9d2ccd5528d743c1ed03742fb08b2eaf795
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2018-11-29 11:00:07 -0700

    started final

--- final/sketch.js
+++ final/sketch.js
@@ -1,1 +1,21 @@
+var song_a;
+//var song_b;
+//var song_c;
 
+
+
+function setup(){
+  createCanvas(300, 300);
+  song_a = loadSound("8-bit_sing_for_ab.mp3", loaded);
+  song_a.setVolume(0.5);
+
+}
+
+function loaded(){
+  song_a.play();
+}
+
+
+function draw(){
+  background(200);
+}
